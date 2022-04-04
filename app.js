1b0e9cdf90a1266a65b47eae6311fdf393c4c348
Ashish Barvaliya <ashish.b@cloudthing.com>
2022-04-04 18:28:57 +0530

    refactoring code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const router = express.Router();
-const User = require("./models/User");
+const usersRouter = require("./routes/users");
+
 require("dotenv").config();
 
 const app = express();
@@ -23,29 +23,4 @@ app.listen(process.env.PORT, () => {
   console.log(`server has started at port ${process.env.PORT}`);
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.find({
-      id: req.body.id,
-      password: req.body.password,
-    });
-    if (user.length) {
-      res.status(200).json({ message: "Logged in" });
-    } else {
-      res.status(400).json({ message: "Invalid user"});
-    }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-app.use("/users", router);
+app.use("/users", usersRouter);
